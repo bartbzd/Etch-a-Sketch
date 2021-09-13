075d5f66aff444e47ad1cd6dd3ebe42d9ce19455
Bart PC <bbart318@gmail.com>
2021-09-12 21:33:00 -0500

    Write function to loop through grid divs

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('test')
+const container = document.querySelector(".container");
+
+function playGrid(col, row) {
+  //16 x 16
+  for (let i = 0; i < col * row; i++) {
+    const div = document.createElement("div");
+    container.appendChild(div);
+    div.style.border = "1px black solid"; //testing
+    div.style.height = "30px";
+    div.style.width = "30px";
+    container.style.gridTemplateColumns = `repeat(${col}, ${div.style.height})`;
+    container.style.gridTemplateRows = `repeat(${row}, ${div.style.height});`;
+    // div.style.float = "left";
+  }
+}
+playGrid(16, 16);
